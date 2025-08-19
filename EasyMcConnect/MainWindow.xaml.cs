0135f2e472a38437992865fd6683c8d74c3113f0
EasyColorful <chensiyuan7788999@163.com>
2025-08-19 15:39:41 +0800

    fist commit

--- EasyMcConnect/MainWindow.xaml.cs
+++ EasyMcConnect/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using Tomlyn;
-using Tomlyn.Model;
 using System.Net;
 using System.Text;
 using System.Windows;
@@ -9,6 +7,9 @@ using System.Windows.Controls;
 using System.Windows.Diagnostics;
 using iNKORE.UI.WPF.Controls;
 using System.IO;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
 
 namespace EasyMcConnect
 {
@@ -18,111 +19,42 @@ namespace EasyMcConnect
     public partial class MainWindow : Window
     {
         private int srcport = new Random().Next(10000, 65535);
-        public static string token = Guid.NewGuid().ToString().Replace("-", "");
+        public string token = "";
+        public FileStream fs = new FileStream("emcc.json", FileMode.Create);
+        
 
         public MainWindow()
         {
             InitializeComponent();
-            textBox4.Text += $"当前电脑主机名:{Dns.GetHostName()}\r\n";
-            textBox5.Text = srcport.ToString();
-            textBox3_frp.Text = srcport.ToString();
-            textBox4_frp.Text += $"当前电脑IP:{Dns.GetHostAddresses(Dns.GetHostName())[0]}\r\n";
-        }
-
-        private  void button1_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                Process p= new();
-                ProcessStartInfo processStartInfo = new ProcessStartInfo();
-                processStartInfo.FileName = "openp2p.exe";
-                string protocol="";
-                switch (comboBox1.SelectedIndex)
-                {
-                    case 0: { protocol = "tcp"; break; }
-                    case 1: { protocol = "udp"; break; }
-                }
-                string arg = $"-node {textBox3.Text} -token 18049001166862278348 -appname {textBox1.Text} -peernode {Dns.GetHostName()} -dstip localhost -dstport {textBox2.Text}  -protocol {protocol} -srcport {srcport}";
-                processStartInfo.Arguments = arg;
-                processStartInfo.UseShellExecute = false;
-                processStartInfo.RedirectStandardError = true;
-                processStartInfo.RedirectStandardOutput = true;
-                processStartInfo.CreateNoWindow = true;
-                Task.Run(() => {
-                    p= Process.Start(processStartInfo);
-                });
-                textBox4.AppendText("Openp2p Client 已启动\r\n");
-                p.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
-                {
-                    if (!String.IsNullOrEmpty(e.Data))
-                    {
-                        textBox4.AppendText(e.Data + "\r\n");
-                    }
-                });
-                p.ErrorDataReceived += new DataReceivedEventHandler((sender, e) =>
-                {
-                    if (!String.IsNullOrEmpty(e.Data))
-                    {
-                        textBox4.AppendText(e.Data + "\r\n");
-                    }
-                });
-                
-
-                //await Task.Run(() => {
-
-                //    while (!p.StandardOutput.EndOfStream || !p.StandardError.EndOfStream)
-                //    {
-                //        this.Dispatcher.Invoke(() =>
-                //        {
-                //            textBox4.AppendText(p.StandardOutput.ReadLine() + "\r\n");
-                //        });
-                //        this.Dispatcher.Invoke(() =>
-                //        {
-                //            textBox4.AppendText(p.StandardError.ReadLine() + "\r\n");
-                //        });
-
-                //    }
-                //});
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void button3_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                Process[] processes = Process.GetProcesses();
-                foreach (Process p in processes)
-                {
-                   
-                    if (p.ProcessName == "openp2p")
-                    {
-                        p.Kill();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void button复制__C__Click(object sender, RoutedEventArgs e)
-        {
-            ProcessStartInfo processStartInfo = new();
-            processStartInfo.FileName = ".\\openp2p.exe";
-            processStartInfo.CreateNoWindow = true;
+            fs.Close();
             
-        }
+            if (!(File.Exists("emcc.json") || (new FileInfo("emcc.json").Length == 0)))
+            {
+                FileStream fs = new FileStream("emcc.json", FileMode.OpenOrCreate);
+                byte[] buffer = new byte[fs.Length];
+                string token = Guid.NewGuid().ToString();
+                buffer = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new Config { token = token }));
+                fs.Write(buffer, 0, buffer.Length);
+                fs.Flush();
+                fs.Close();
+            }
+            else
+            {
+                FileStream fs2 = new FileStream("emcc.json", FileMode.Open);
+                byte[] buffer2 = new byte[fs2.Length];
+                fs2.Read(buffer2, 0, buffer2.Length);
+                Config config = JsonConvert.DeserializeObject<Config>(Encoding.UTF8.GetString(buffer2));
+                this.token = config.token;
+                fs2.Close();
+            }
 
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            textBox4.Clear();
-        }
+
+
+
+            }
+
+            
+
 
         private void button1_frp_Click(object sender, RoutedEventArgs e)
         {
@@ -140,11 +72,14 @@ namespace EasyMcConnect
                 FileStream fs = new FileStream("frpc.toml", FileMode.Create);
                 byte[] buffer = new byte[fs.Length];
                 fs.Read(buffer, 0, buffer.Length);
+                FileStream fs2 = new FileStream("emcc.json", FileMode.Open);
+                byte[] buffer2 = new byte[fs2.Length];
+                fs2.Read(buffer2, 0, buffer2.Length);
                 var toml = $@"serverAddr = ""154.7.177.37""
 auth.token = ""wy780104""
 serverPort = 7000
 [[proxies]]
-name = ""game{token}""
+name = ""game.{token}""
 type = ""{comboBox1_frp.Text.ToLower()}""
 localIP = ""127.0.0.1""
 localPort = {textBox1_frp.Text}
@@ -154,24 +89,31 @@ remotePort = {textBox3_frp.Text}";
                 fs.Flush();
                 fs.Close();
 
-                   p = Process.Start(processStartInfo);
-               
-                textBox4.AppendText("frpc 已启动\r\n");
+                p = Process.Start(processStartInfo);
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+                textBox4_frp.AppendText("frpc 已启动\r\n");
                 p.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
                 {
                     if (!String.IsNullOrEmpty(e.Data))
                     {
-                        textBox4_frp.AppendText(e.Data + "\r\n");
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            textBox4_frp.AppendText(e.Data + "\r\n");
+                        });
                     }
                 });
                 p.ErrorDataReceived += new DataReceivedEventHandler((sender, e) =>
                 {
                     if (!String.IsNullOrEmpty(e.Data))
                     {
-                        textBox4_frp.AppendText(e.Data + "\r\n");
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            textBox4_frp.AppendText(e.Data + "\r\n");
+                        });
                     }
                 });
-                
+
             }
             catch (Exception ex)
             {
@@ -179,11 +121,7 @@ remotePort = {textBox3_frp.Text}";
             }
 
         }
-        public static Proxies Parse(string toml)
-        {
-            var model = Toml.ToModel<Proxies>(toml);
-            return model;
-        }
+
 
         private void button2_frp_Click(object sender, RoutedEventArgs e)
         {
@@ -210,14 +148,79 @@ remotePort = {textBox3_frp.Text}";
             textBox4_frp.Clear();
 
         }
-    }
-    public class Proxies
-    {
-        public string name { get; set; }
-        public string type { get; set; }
-        public string localIP { get; set; }
-        public int localPort { get; set; }
-        public int remotePort { get; set; }
 
+        private void button1_et_Click(object sender, RoutedEventArgs e)
+        {
+            Process p = new();
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = "easytier-core.exe";
+            processStartInfo.Arguments = $"-d --network-name {textBox1_et.Text} --network-secret {textBol2_et.Text} -p tcp://154.7.177.37:11010";
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.CreateNoWindow = true;
+            p = Process.Start(processStartInfo);
+            textBox4_et.AppendText("EasyTier 已启动\r\n");
+            p.BeginOutputReadLine();
+            p.BeginErrorReadLine();
+            p.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+            {
+                if (!String.IsNullOrEmpty(e.Data))
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        textBox4_et.AppendText(e.Data + "\r\n");
+                        Regex IPAd = new Regex(@"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b");
+                        MatchCollection MatchResult = IPAd.Matches(e.Data);
+                        textBox3_et.Text = MatchResult[0].Value;
+                    });
+                }
+            });
+            p.ErrorDataReceived += new DataReceivedEventHandler((sender, e) =>
+            {
+                if (!String.IsNullOrEmpty(e.Data))
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        textBox4_et.AppendText(e.Data + "\r\n");
+                    });
+                }
+            });
+        }
+        
+
+        private void button3_et_Click(object sender, RoutedEventArgs e)
+        {
+              textBox4_et.Clear();
+        }
+
+        private void button2_et_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process[] processes = Process.GetProcesses();
+                foreach (Process p in processes)
+                {
+
+                    if (p.ProcessName == "easytier-core")
+                    {
+                        p.Kill();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
+}
+class Config
+{
+    public string token { get; set; }
 }
